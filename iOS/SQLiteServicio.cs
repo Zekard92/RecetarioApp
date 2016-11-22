3ceaa1db62be648ba52d9ef2fbcb6fbf23a1ae3e
Miguel (Zekard) Guajardo <mguajardoal@gmail.com>
2016-11-22 10:57:46 -0600

    IOS
    The IOS project is now completed

--- iOS/SQLiteServicio.cs
+++ iOS/SQLiteServicio.cs
@@ -1,11 +1,11 @@
-﻿using RecetarioApp.Droid;
-using SQLite.Net;
+﻿using SQLite.Net;
 using Xamarin.Forms;
 using System;
 using System.IO;
+using RecetarioApp.iOS;
 
 [assembly: Dependency(typeof(SQLiteServicio))]
-namespace RecetarioApp.Droid
+namespace RecetarioApp.iOS
 {
 	public class SQLiteServicio : ISQLite
 	{
@@ -13,12 +13,11 @@ namespace RecetarioApp.Droid
 		{
 			var archivoBaseDatos = "Recetas.db3";
 			string directorioBaseDatos = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-			string rutaCompleta = Path.Combine(directorioBaseDatos, archivoBaseDatos);
+			string rutaLibreria = Path.Combine(directorioBaseDatos, "..", "Library");
+			var rutaCompleta = Path.Combine(rutaLibreria, archivoBaseDatos);
 			if (!File.Exists(rutaCompleta))
-			{
 				File.Create(rutaCompleta);
-			}
-			var plataforma = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+			var plataforma = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
 			var conexion = new SQLiteConnection(plataforma, rutaCompleta);
 			return conexion;
 		}
